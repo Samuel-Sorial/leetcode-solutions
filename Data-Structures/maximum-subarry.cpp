6f6a2301ae93b95fbac72eb445c236f24d90a8bf
Samuel Sorial <samuelmagedsorial@gmail.com>
2022-06-29 20:23:40 +0200

    [Algorithms] Implementing maximum subarry sum using dynamic programming

---
+++ Data-Structures/maximum-subarry.cpp
@@ -0,0 +1,22 @@
+
+#include <vector>
+using std::vector;
+
+// https://leetcode.com/problems/maximum-subarray/
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        // for each number, check previous sum, if higher than previous sum + itself, set it as previous sum + itself, else, set number as current
+        int currentSum = nums[0];
+        int maximum = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            currentSum = std::max(currentSum + nums[i], nums[i]);
+            maximum = std::max(maximum, currentSum);
+        }
+        return maximum;
+    }
+};
