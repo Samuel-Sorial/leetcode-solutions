eddedfb718f852d162943de30f3dcc5a6b544b46
Samuel Sorial <samuelmagedsorial@gmail.com>
2022-06-29 19:03:00 +0200

    [Algorithms] Implementing contains duplicate using unordered_set

--- Data-Structures/contains-duplicate.cpp
+++ Data-Structures/contains-duplicate.cpp
@@ -1,11 +1,27 @@
 #include <vector>
+#include <unordered_set>
+using std::unordered_set;
 using std::vector;
 
-// https://leetcode.com/problems/contains-duplicate/
+// https:// leetcode.com/problems/contains-duplicate
+
 class Solution
 {
 public:
     bool containsDuplicate(vector<int> &nums)
     {
+        unordered_set<int> elements;
+        for (int number : nums)
+        {
+            if (elements.find(number) == elements.end())
+            {
+                elements.insert(number);
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
