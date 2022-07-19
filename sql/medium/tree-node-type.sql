b8b523b3816e43c0fd9463a9d16787b89c58c8cb
Samuel Sorial <samuelmagedsorial@gmail.com>
2022-07-20 01:18:05 +0200

    [SQL] Implementing tree node type

---
+++ sql/medium/tree-node-type.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/tree-node
+SELECT id,
+CASE 
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
+    ELSE 'Leaf'
+END AS type
+FROM Tree
